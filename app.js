46e46972a34b00023d503cde668bc366b07ca0fd
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-06-12 23:40:13 -0700

    separating seeding and listening into separate modules

--- app.js
+++ app.js
@@ -6,7 +6,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
 
 // support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,38 +17,15 @@ app.use(bodyParser.json());
 // Routing information
 /* ====================================== */
 
+const deliRoutes = require('./api/routes/delis');
 
-const db = require('./api/data/db');
-const data = require('./api/data/delis');
+app.use('/', deliRoutes);
 
-db.connect().then(function(db){
-    db.collection('delis')
-        .insertMany([
-            data.margon,
-            data.juniors,
-            data.melt_shop,
-            data.piccolo,
-            data.toasties
-        ], 
-        function (err, r) {
-            // assert.equal(null, err);
-            // assert.equal(5, r.insertedCount);
-            console.log("Database setup with five delis.");
+app.use((request, response, next) => {
+    response.status(200).json({
+        message: "You're becoming a hero!"
+    });
+});
 
-            const deliRoutes = require('./api/routes/delis');
-
-            app.use('/', deliRoutes);
-
-            app.use((request, response, next) => {
-                response.status(200).json({
-                    message: "You're becoming a hero!"
-                });
-            });
-
-            // Start the web server
-            app.listen(port, () => {
-                console.log('Express.js server is listening on Port %s.', port);
-            });
-        });
-})
-
+// app separated for intergation testing and separate seeding
+module.exports = app;
